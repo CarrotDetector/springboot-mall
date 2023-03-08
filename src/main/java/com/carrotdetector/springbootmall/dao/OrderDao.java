1f1f8aa138c1d9240977854667f680c8ca7ea253
Carrot Detector <bokuxmeatball@gmail.com>
2023-03-08 23:37:12 +0800

    訂單功能 - 創建訂單(下)

--- src/main/java/com/carrotdetector/springbootmall/dao/OrderDao.java
+++ src/main/java/com/carrotdetector/springbootmall/dao/OrderDao.java
@@ -1,10 +1,16 @@
 package com.carrotdetector.springbootmall.dao;
 
+import com.carrotdetector.springbootmall.model.Order;
 import com.carrotdetector.springbootmall.model.OrderItem;
 
 import java.util.List;
 
 public interface OrderDao {
+    Order getOrderById(Integer orderId);
+
+    List<OrderItem> getOrderItemsByOrderId(Integer orderId);
+
+
     Integer createOrder(Integer userId, Integer totalAmount);
 
     void createOrderItems(Integer orderId, List<OrderItem> orderItemList);
--- src/main/java/com/carrotdetector/springbootmall/service/OrderService.java
+++ src/main/java/com/carrotdetector/springbootmall/service/OrderService.java
@@ -1,7 +1,10 @@
 package com.carrotdetector.springbootmall.service;
 
 import com.carrotdetector.springbootmall.dto.CreateOrderRequest;
+import com.carrotdetector.springbootmall.model.Order;
 
 public interface OrderService {
+    Order getOrderById(Integer orderId);
+
     Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest);
 }
